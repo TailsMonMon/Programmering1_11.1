1a7e27db16a82c0d48d06408fc2ec2e2322b0135
Monica Björk <m.bjork95@gmail.com>
2018-04-19 16:30:10 +0200

    Add project files.

---
+++ Programmering1_11.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_11._1 {
+    class Program {
+        static void Main(string[] args) {
+            Hello("test");
+        }
+
+        static string Hello(string fromMain) {
+            string myMethod = "Hey ho lets go" + fromMain;
+            return myMethod;
+        }
+    }
+}
