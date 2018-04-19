3b6a82a1b0229961e9d4848fab26a49f509129c0
Monica Björk <m.bjork95@gmail.com>
2018-04-19 17:09:26 +0200

    Done!

--- Programmering1_11.1/Program.cs
+++ Programmering1_11.1/Program.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 namespace Programmering1_11._1 {
     class Program {
         static void Main(string[] args) {
-            Hello("test");
+            Console.WriteLine(Hello("this is working!")); // The string is sent to the method Hello()
+            Console.ReadLine();
         }
 
+        //      A method to combine two strings 
         static string Hello(string fromMain) {
-            string myMethod = "Hey ho lets go" + fromMain;
+            string myMethod = "Hey ho lets go, " + fromMain;
             return myMethod;
         }
     }
